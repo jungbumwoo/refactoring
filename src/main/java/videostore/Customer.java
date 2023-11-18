1f6b38ca29a842de744c16b759517a6036d95add
matthew.woo <jungbumwoo@naver.com>
2023-11-18 16:35:39 +0900

    :recycle: Decomposing and Redistributing the statement method

--- src/main/java/videostore/Customer.java
+++ src/main/java/videostore/Customer.java
@@ -21,10 +21,20 @@ public class Customer {
 
     /*
     *
+    * Initial Comment
+    *
     * Statement routine does far too much
     * Many of the things that it does should really be done by the other classes.
     * 변경이 생길 경우, 기존 코드는 재사용될 수 없음
     * 변경하고자 할 때, 여러 선택지가 존재할 것이고 이로 인해 다른 부분에도 영향을 주거나, 계속해서 변경할 수 밖에 없고 이로 인한 리소스 소모
+    * */
+
+    /*
+    * Decomposing and Redistributing the Statement Method
+    *
+    * First I need to look in the fragment for any variables that are local in scope to the method we are looking at, the local variables and parameters.
+    * This segment of code uses two: `each` and `thisAmount`.
+    *
     * */
     public String statement(){
         double totalAmount = 0;
@@ -36,23 +46,7 @@ public class Customer {
             Rental each = (Rental) rentals.nextElement();
 
             //determine amounts for each line
-            switch (each.getMovie().getPriceCode()) {
-                case Movie.REGULAR:
-                    thisAmount += 2;
-                    if (each.getDaysRented() > 2)
-                        thisAmount += (each.getDaysRented() - 2) * 1.5;
-                    break;
-
-                case Movie.NEW_RELEASE:
-                    thisAmount += each.getDaysRented() * 3;
-                    break;
-
-                case Movie.CHILDRENS:
-                    thisAmount += 1.5;
-                    if (each.getDaysRented() > 3)
-                        thisAmount += (each.getDaysRented() - 3) * 1.5;
-                    break;
-            }
+            thisAmount = amountFor(each);
 
             // add frequent renter points
             frequentRenterPoints ++;
@@ -65,12 +59,34 @@ public class Customer {
             result += "\t" + each.getMovie().getTitle()+ "\t" +
                     String.valueOf(thisAmount) + "\n";
             totalAmount += thisAmount;
-
         }
 
         // add footer lines
         result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
         result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
         return result;
+    }
+
+    // thisAmount가 기존 함수(local variable)에서 수정되는 것을 분리하였음
+    private double amountFor(Rental each) {
+        double thisAmount = 0;
+        switch (each.getMovie().getPriceCode()){
+            case Movie.REGULAR:
+                thisAmount += 2;
+                if(each.getDaysRented() > 2)
+                    thisAmount += (each.getDaysRented() -2) * 1.5;
+                break;
+
+            case Movie.NEW_RELEASE:
+                thisAmount += each.getDaysRented() * 3;
+                break;
+
+            case Movie.CHILDRENS:
+                thisAmount += 1.5;
+                if(each.getDaysRented() > 3)
+                    thisAmount += (each.getDaysRented() -3) * 1.5;
+                break;
+        }
+        return thisAmount;
     }
 }
--- summary.md
+++ summary.md
@@ -8,3 +8,7 @@ If you don't, you end up spending time hand checking some numbers from the test 
 
 **Before you start refactoring, check that you have a solid suite of tests. These tests must be self-checking.**
 
+### Decomposing and Refactoring
+
+Tip. Refactoring changes the programs in small steps. If you make a mistake, it is easy to find the bug.
+
