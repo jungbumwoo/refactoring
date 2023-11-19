4352a47f3eb47ce61f18f7b3d52b09939a7b3bb9
matthew.woo <jungbumwoo@naver.com>
2023-11-19 15:56:26 +0900

    Replacing the Conditional Logic on Price Code with Polymorphism

--- src/main/java/videostore/Movie.java
+++ src/main/java/videostore/Movie.java
@@ -24,4 +24,35 @@ public class Movie {
     public String getTitle() {
         return _title;
     }
+
+    //  In most cases a method should be on the object whose data it uses, thus the method should be moved to the rental class.
+    double getCharge(int daysRented) {
+        double result = 0;
+        switch (getPriceCode()){
+            case Movie.REGULAR:
+                result += 2;
+                if(daysRented > 2)
+                    result += (daysRented -2) * 1.5;
+                break;
+
+            case Movie.NEW_RELEASE:
+                result += daysRented * 3;
+                break;
+
+            case Movie.CHILDRENS:
+                result += 1.5;
+                if(daysRented > 3)
+                    result += (daysRented -3) * 1.5;
+                break;
+        }
+        return result;
+    }
+
+    int getFrequentRenterPoints(int daysRented) {
+        if ((getPriceCode() == Movie.NEW_RELEASE)
+                &&
+                daysRented > 1) return 2;
+        else
+            return 1;
+    }
 }
--- summary.md
+++ summary.md
@@ -25,3 +25,14 @@ Moving the Amount Calculation
 I like to get rid of temporary variables such as this as much as possible. 
 Temps are often a problem in that they cause a lot of parameters to be passed around when they don't have to be. 
 You can easily lose track of what they are there for. They are particularly insidious in long methods.
+
+
+### Replacing the Conditional Logic on Price Code with Polymorphism
+
+The first part of this problem is that switch statement. It is a bad idea to do a switch based on an attribute of another object.
+If you must use a switch statement, it should be on your own data, not on someone else's.
+
+Why do I prefer to pass the length of rental to the movie rather than the movie type to the rental? 
+It's because the proposed changes are all about adding new types. Type information generally tends to be more volatile. 
+If I change the movie type, I want the least ripple effect, 
+so I prefer to calculate the charge within the movie.
